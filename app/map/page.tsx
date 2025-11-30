c757e289613bbee295fea7e30a62ae2e513cbb12
jianbaek <jian0314@yonsei.ac.kr>
2025-12-01 03:38:05 +0900

    feat: 매장 지도 페이지에 map.png 이미지 적용

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -129,26 +129,13 @@ export default function MapPage() {
         </div>
       </div>
 
-      {/* Image Placeholder */}
+      {/* Map Image */}
       <div className={styles.imagePlaceholder}>
-        <div className={styles.placeholderContent}>
-          <svg
-            width="48"
-            height="48"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M4 16L8.586 11.414C9.367 10.633 10.633 10.633 11.414 11.414L16 16M14 14L15.586 12.414C16.367 11.633 17.633 11.633 18.414 12.414L22 16M14 8H14.01M6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20Z"
-              stroke="#999"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          <span>이미지</span>
-        </div>
+        <img 
+          src="/map.png" 
+          alt="매장 지도" 
+          className={styles.mapImage}
+        />
       </div>
 
       {/* Content Section */}
